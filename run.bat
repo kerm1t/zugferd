13c4f55a3e40c0a8c46e0dc7bbfe67ad41e954b3
kerm1t <wolfgang.schulz@gmail.com>
2024-10-01 09:25:19 +0200

    test files

---
+++ run.bat
@@ -0,0 +1,3 @@
+set gspath=c:\winapp\gs10.04.0\bin
+set docpath=C:\GIT\zugferd/
+%gspath%\gswin64 --permit-file-read=%docpath% -sDEVICE=pdfwrite -dPDFA=3 -sColorConversionStrategy=RGB -sZUGFeRDXMLFile=%docpath%\Musterrechnung_einfach_basic.xml -sZUGFeRDProfile=%docpath%\default_rgb.icc -sZUGFeRDVersion=2p1 -sZUGFeRDConformanceLevel=BASIC -o invoice_out.pdf zugferd.ps invoice.pdf
